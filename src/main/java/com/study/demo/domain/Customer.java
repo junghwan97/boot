6b8be24a7fba721042c9e18e7433b18212c3a7a6
junghwan97 <jhzzang97@gmail.com>
2023-04-17 08:29:04 +0900

    like, preparedstatement

---
+++ src/main/java/com/study/demo/domain/Customer.java
@@ -0,0 +1,11 @@
+package com.study.demo.domain;
+
+import lombok.*;
+
+@Data
+public class Customer {
+	private int id;
+	private String name;
+	private String address;
+	private String contactName;
+}
