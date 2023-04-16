af9c66ab3ed12924a4612b2822077bf86965d54d
junghwan97 <jhzzang97@gmail.com>
2023-04-17 08:28:17 +0900

    spring.datasource.name -> spring.datasource.username

--- src/main/java/com/example/demo/controller/Controller11.java
+++ src/main/java/com/example/demo/controller/Controller11.java
@@ -18,63 +18,49 @@ public class Controller11 {
 
 	@Value("${spring.datasource.url}")
 	private String url;
-	@Value("${spring.datasource.name}")
+	@Value("${spring.datasource.username}")
 	private String name;
 	@Value("${spring.datasource.password}")
 	private String password;
 
 	@RequestMapping("link1")
 	public void method1(Model model) {
-		// 1. request param 수집
+		// 1. request param
 
 		// 2. business logic
-		// 아이디가 1인 고객 이름 조회
 
+		// 아이디 1인 고객 이름 조회
 		// SELECT CustomerName FROM Customers WHERE CustomerID = 1;
-
 		String sql = "SELECT CustomerName FROM Customers WHERE CustomerID = 1";
-		try (
-				// db 연결
-				Connection connection = DriverManager.getConnection(url, name, password);
-				// Statement 객체 생성
-				Statement statement = connection.createStatement();
-				// 쿼리 실행
-				// 쿼리 실행 결과 얻고
-				ResultSet resultSet = statement.executeQuery(sql);) {
-			// 쿼리 실행 결과 가공
+
+		try (Connection con = DriverManager.getConnection(url, name, password);
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);) {
 			String name = "";
-			if (resultSet.next()) {
-				name = resultSet.getString("CustomerName");
+			if (rs.next()) {
+				name = rs.getString("CustomerName");
+
 			}
 
-			// 3. add attribute
 			model.addAttribute("customerName", name);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		// 4. forward
 	}
 
 	@RequestMapping("link2")
-	public void method2() {
-		String employeeName = "";
-		// 2. business logic
-		// 아이디가 3인 직원의 LastName 조회
+	public void method2(Model model) {
 
-		String sql = "SELECT LastName FROM Employees WHERE EmployeeId = 3";
-		try (
-				// 1. 연결하기
-				Connection con = DriverManager.getConnection(url, name, password);
-				// 2. statement 생성
+		String employeeName = "";
+
+		String sql = "SELECT lastName FROM Employees WHERE EmployeeId = 3";
+		try (Connection con = DriverManager.getConnection(url, name, password);
 				Statement stmt = con.createStatement();
-				// 3. 쿼리 실행 후 resultSet 얻기
-				ResultSet rs = stmt.executeQuery(sql);
-		// 4. resultSet에 결과 있는 지 확인하고
-		) {
+				ResultSet rs = stmt.executeQuery(sql);) {
 			if (rs.next()) {
-				// 5. 결과 꺼내서 담기
-				employeeName = rs.getString("LastName");
+				employeeName = rs.getString("lastName");
 			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -83,81 +69,80 @@ public class Controller11 {
 
 	@RequestMapping("link3")
 	public void method3(Model model) {
-		// 업무
-		// 아이디가 5인 상품 이름 조회
-		String sql = """
-				SELECT ProductName
-				FROM Products
-				WHERE ProductID = 5
-				""";
-		try (
-				// 1. 연결하기
-				Connection con = DriverManager.getConnection(url, name, password);
-				// 2. statement 생성
+
+		String productName = "";
+		String sql = "SELECT ProductName FROM Products WHERE ProductID = 5";
+
+		try (Connection con = DriverManager.getConnection(url, name, password);
 				Statement stmt = con.createStatement();
-				// 3. 쿼리 실행 후 resultSet 얻기
-				ResultSet rs = stmt.executeQuery(sql);
-		// 4. resultSet에 결과 있는지 확인하고
-		) {
-			String productName = "";
+				ResultSet rs = stmt.executeQuery(sql);) {
 			if (rs.next()) {
-				// 5. 결과 꺼내서 담기
-				productName = rs.getString("ProductName");
+				productName = rs.getString("productName");
 			}
-			model.addAttribute("ProductName", productName);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		model.addAttribute("ProductName", productName);
 	}
 
 	@RequestMapping("link4")
 	public void method4(Model model) {
-		// 업무
+		// #업무
 		// 고객 이름들 조회
 
-		String sql = "SELECT CustomerName FROM Customers";
-		List<String> customerList = new ArrayList<>();
+		String sql = """
+				SELECT CustomerName
+				FROM Customers
+				""";
+
+		List<String> list = new ArrayList<>();
+
 		try {
-			// 1. 연결하기
 			Connection con = DriverManager.getConnection(url, name, password);
-			// 2. Statement 생성
 			Statement stmt = con.createStatement();
-			// 3. 쿼리 실행 후 resultSet 얻기
 			ResultSet rs = stmt.executeQuery(sql);
-			// 4. resultSet에 결과 있는지 확인하고
+
 			try (con; stmt; rs;) {
+				// list에 고객 이름들을 담고
 				while (rs.next()) {
-					// 5. 결과 꺼내서 담기
-					customerList.add(rs.getString("CustomerName"));
+					list.add(rs.getString("CustomerName"));
 				}
 			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		model.addAttribute("CustomerNames", customerList);
+
+		// 3. add attribute
+		model.addAttribute("customerNames", list);
+
+		// 4. forward
 	}
 
 	@RequestMapping("link5")
 	public void method5(Model model) {
-		String sql = "SELECT firstName FROM Employees";
+		// 직원 이름(firstName)들을 조회해서 jsp로 출력
+		String sql = """
+				SELECT firstName
+				FROM Employees
+				""";
+
 		List<String> list = new ArrayList<>();
+
 		try {
-			// 1. 연결하기
 			Connection con = DriverManager.getConnection(url, name, password);
-			// 2. Statement 생성
 			Statement stmt = con.createStatement();
-			// 3. 쿼리 실행 후 ResultSet 얻기
 			ResultSet rs = stmt.executeQuery(sql);
-			// 4. resultSet에 결과 있는지 확인하고
+
 			try (con; stmt; rs;) {
 				while (rs.next()) {
-					// 5. 결과 꺼내서 담기
 					list.add(rs.getString("firstName"));
 				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		model.addAttribute("firstName", list);
+
+		model.addAttribute("names", list);
 	}
 }
