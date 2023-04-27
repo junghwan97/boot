7879e766a3a22ad3d558927d227e741a6714db1b
junghwan97 <jhzzang97@gmail.com>
2023-04-27 16:52:13 +0900

    mybatis

--- src/main/java/com/study/demo/domain/Employee.java
+++ src/main/java/com/study/demo/domain/Employee.java
@@ -1,5 +1,7 @@
 package com.study.demo.domain;
 
+import java.time.LocalDate;
+
 import lombok.Data;
 
 @Data
@@ -7,4 +9,7 @@ public class Employee {
 	private int id;
 	private String lastName;
 	private String firstName;
+	private LocalDate birth;
+	private String photo;
+	private String notes;
 }
