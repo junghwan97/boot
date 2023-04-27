5611e34320a36a487ebc04be8477721167049f06
junghwan97 <jhzzang97@gmail.com>
2023-04-27 16:51:57 +0900

    auto increment

---
+++ src/main/java/com/study/demo/domain/Dto11.java
@@ -0,0 +1,11 @@
+package com.study.demo.domain;
+
+import lombok.Data;
+
+@Data
+public class Dto11 {
+	
+	private Integer prop1;
+	private String prop2;
+	private Integer prop3;
+}
---
+++ src/main/java/com/study/demo/domain/Dto12.java
@@ -0,0 +1,12 @@
+package com.study.demo.domain;
+
+import lombok.Data;
+
+@Data
+public class Dto12 {
+
+	private Integer id;
+	private Integer age;
+	private String name;
+	private Double score;
+}
